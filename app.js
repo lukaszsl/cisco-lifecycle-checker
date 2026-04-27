8383d65448a547351f759ff3cc414fca14e8f8ab
Lukasz Sliczner <lukasz.sliczner@interia.pl>
2026-04-27 18:48:59 +0000

    Refactor server setup and add environment configuration

--- app.js
+++ app.js
@@ -3,9 +3,12 @@
    INITIAL EXPRESS CONFIG  
 
 -------------------------- */
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 // set EJS as template engine
 
@@ -14,15 +17,11 @@ app.set("view engine", "ejs");
 // basic route
 
 app.get("/", (req, res) => {
-
   res.send("Cisco Lifecycle Checker - running");
-
 });
 
 // start server
 
-app.listen(3000, () => {
-
-  console.log("Server running on http://localhost:3000");
-
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
