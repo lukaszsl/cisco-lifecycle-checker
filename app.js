5457865fe0c4b3b0a3284c3f7d4facc6e4318085
Lukasz Sliczner <lukasz.sliczner@interia.pl>
2026-04-28 17:37:27 +0000

    Add PID form and handle form submission

--- app.js
+++ app.js
@@ -14,10 +14,21 @@ const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
+// // parse URL-encoded form data so submitted form fields are available in req.body
+
+app.use(express.urlencoded({ extended: true }));
+
 // basic route
 
 app.get("/", (req, res) => {
-  res.send("Cisco Lifecycle Checker - running");
+  res.render("index");
+});
+
+// handle submitted device check form and read user input from req.body
+
+app.post("/check", (req, res) => {
+    console.log(req.body); // TODO: Remove in the final stage
+    res.send(`You entered PID: ${req.body.pid}`);
 });
 
 // start server
